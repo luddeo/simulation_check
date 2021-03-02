42e48b2b0ced82d65f8a9d2a9058cd050172527a
luddeo <privat@eriklundin.nu>
2021-03-02 21:53:59 +0100

    Reorder and add threshold parameter.

--- simulation_class.py
+++ simulation_class.py
@@ -2,18 +2,17 @@
 
 ### TODO:
 ###  - give the exception a good text
-###  - Need a time threshold to funcitons giving pOcc, DT, D
-###  - add caching, try lru_cache that is imported below or use somethign else.
-
+###  - add caching, try @lru_cache:
+###      from functools import lru_cache
 
 import pandas as pd
+import numpy as np
 import os
-from functools import lru_cache
+
 
 class MesoRDsimulation:
-
 	def __init__(self, sim_directory, cube_size):
-		''' Reads in a MesoRD simulations in sim_directory.
+		''' Reads in a MesoRD simulation in sim_directory.
 	
 		'''
 		if (not os.path.isdir(sim_directory)):
@@ -27,77 +26,85 @@ class MesoRDsimulation:
 		self.IDs = trajectories.iloc[:,1::5]
 		self.species = trajectories.iloc[:,2::5]
 		self.species.columns =  [str(i) for i in range(len(self.species.columns))]
-		self.pOcc_m = self.species.apply(pd.Series.value_counts, axis=1, normalize=True).apply(pd.Series.mean)
-		self.pOcc_sd = self.species.apply(pd.Series.value_counts, axis=1, normalize=True).apply(pd.Series.std)
 		
-		x_coord = trajectories.iloc[:,3::5]
-		x_coord.columns =  [str(i) for i in range(len(x_coord.columns))]
-		y_coord = trajectories.iloc[:,4::5]
-		y_coord.columns =  [str(i) for i in range(len(y_coord.columns))]
-		z_coord = trajectories.iloc[:,5::5]
-		z_coord.columns =  [str(i) for i in range(len(z_coord.columns))]
+		self.x_coord = trajectories.iloc[:,3::5]
+		self.x_coord.columns =  [str(i) for i in range(len(self.x_coord.columns))]
+		self.y_coord = trajectories.iloc[:,4::5]
+		self.y_coord.columns =  [str(i) for i in range(len(self.y_coord.columns))]
+		self.z_coord = trajectories.iloc[:,5::5]
+		self.z_coord.columns =  [str(i) for i in range(len(self.z_coord.columns))]
 		
-		
-		D_df = cube_size*cube_size*(x_coord.diff().iloc[1:,:].pow(2) + y_coord.diff().iloc[1:,:].pow(2) + z_coord.diff().iloc[1:,:].pow(2)).divide(6*self.time.diff().iloc[1:], axis= 'rows')
-		D_longdf = pd.DataFrame({'species': self.species.iloc[1:,:].melt().value, 'x': D_df.melt().value})
-		# Now just using the first species in each interval, would rater us ehen the interval is all the same specis
-		#species.iloc[1:,:] # remove first row 
-		#species.iloc[0:(-1),:] # remove last row
-		# Need to compare the two and only use then both are the same.
-		self.D_m = D_longdf.groupby('species').mean()
-		self.D_sd = D_longdf.groupby('species').std()
+		self.species_order = self.species.melt().value.unique()
 		
 		# some random things done when trying to calculate things
-		# formula for diffusion: cs^2 * dx2 / (6*dt)
-		#x_coord.divide(time,axis='rows')
-		#pd.DataFrame({'species': species.melt().value, 'x': x_coord.melt().value})
-		#pd.DataFrame({'species': species.melt().value, 'x': x_coord.melt().value}).groupby('species').mean()
-		
-
-		# species   get unique: .melt().iloc[:,1].unique()
-		# X  b1 = uu.iloc[:,3::5].diff().iloc[1:,:].pow(2)  ; b1.columns =  [str(i) for i in range(500)]
-		# .iloc[1:,:] removes the first row with NaN in it
-		
-		
 		#uu2 = pd.read_csv('simulation_example/reactions.txt',sep = ' ', header=None)
 		#uu2.iloc[:,2].unique() # species
 
-	@lru_cache
 	def get_species(self):
-		''' return a list of the different species in the simulation. unsorted
+		''' return a list of the different species in the simulation. Need not be sorted
 		'''
-		return [i for i in self.pOcc_m.index]
+		return self.species_order
 
-	@lru_cache
-	def get_pOcc_mean(self):
+	def set_species_order(self, value):
+		if (np.array_equal(np.sort(self.species_order), np.sort(value))):
+			self.species_order = value
+		else:
+			raise Exception("the elements does not match")#Need a more specific exception
+
+	def get_pOcc_mean(self, threshold = 0):
 		'''
 		'''
-		return self.pOcc_m
-	
-	def get_pOcc_sd(self):
+		return self.species.iloc[self.time.values >= threshold, :].apply(pd.Series.value_counts, axis=1, normalize=True).apply(pd.Series.mean).loc[self.species_order]
+
+	def get_pOcc_sd(self, threshold = 0):
 		'''
 		'''
-		return self.pOcc_sd
+		return self.species.iloc[self.time.values >= threshold, :].apply(pd.Series.value_counts, axis=1, normalize=True).apply(pd.Series.std).loc[self.species_order]
+
+	def __get_D_values__(self, threshold):
+		'''
+		'''
+		# Formula for 3D-diffusion: D = cs^2 * dx2 / (6*dt)
+		# Now just using the first species in each interval, would rather use the intervals where it stays the same species
+		#species.iloc[1:,:] # remove first row 
+		#species.iloc[0:(-1),:] # remove last row
+		# Need to compare the two and only use then both are the same.
+		D_values = self.cube_size*self.cube_size* (
+			self.x_coord.iloc[self.time.values >= threshold, :].diff().iloc[1:,:].pow(2) + 
+			self.y_coord.iloc[self.time.values >= threshold, :].diff().iloc[1:,:].pow(2) +
+			self.z_coord.iloc[self.time.values >= threshold, :].diff().iloc[1:,:].pow(2)
+			).divide(6*self.time.iloc[self.time.values >= threshold].diff().iloc[1:], axis= 'rows')
+		return pd.DataFrame({'species': self.species.iloc[self.time.values >= threshold, :].iloc[1:,:].melt().value, 'x':D_values.melt().value})
+
+	def get_D_mean(self, threshold = 0):
+		'''
+		'''
+		return self.__get_D_values__(threshold).groupby('species').mean().loc[self.species_order]
+
+	def get_D_sd(self, threshold = 0):
+		'''
+		'''
+		return self.__get_D_values__(threshold).groupby('species').std().loc[self.species_order]
 		
-	def get_D_mean(self):
-		'''
-		'''
-		return self.D_m
-	
-	def get_D_sd(self):
-		'''
-		'''
-		return self.D_sd
-		
-# for thesting purposes
+# for testing purposes
 if __name__ == "__main__":
 	new_sim = MesoRDsimulation('simulation_example',0.01)
 	print(new_sim.get_species())
-	print("pOcc")
-	print(new_sim.get_pOcc_mean())
-	print(new_sim.get_pOcc_sd())
-	print("D")
+	#print("pOcc")
+	#print(new_sim.get_pOcc_mean())
+	#print(new_sim.get_pOcc_mean(20))
+	#print(new_sim.get_pOcc_sd())
+	#print(new_sim.get_pOcc_sd(20))
+	#print("D")
 	print(new_sim.get_D_mean())
 	print(new_sim.get_D_sd())
+
+	print(new_sim.get_D_mean(20))
+	print(new_sim.get_D_sd(20))
+
+	#print(new_sim.get_pOcc_mean())
+	#new_sim.set_species_order(["Aa","Bb","Cc"])
+	#print(new_sim.get_species())
+	#print(new_sim.get_pOcc_mean())
 	# should give an exception
 	#new_sim = MesoRDsimulation('simulation_ample','0.01')
